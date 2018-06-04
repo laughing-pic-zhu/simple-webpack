3a5ecd7372876da2a3046a71cf7073445edcdb9e
qianyi.zj <qianyi.zj@alibaba-inc.com>
2018-06-04 15:33:40 +0800

    resolve增加文件索引函数注释,方便拓展loader函数

--- example/main.js
+++ example/main.js
@@ -1,13 +1,2 @@
 require('d');
-
-function a() {
-    require.ensure(['./a'], function () {
-        require('c');
-    });
-}
-
-require.ensure(['./b'], function () {
-    require('./m');
-});
-
-require('./e');
+require('./test2.json');
