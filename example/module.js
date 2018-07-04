422710ef0ffeccd4e5210a3d5e55ed8a870d7771
qianyi.zj <qianyi.zj@alibaba-inc.com>
2018-07-04 15:30:38 +0800

    add replaceSource,RawSource.edit template,parserplugin to support generate result

--- example/module.js
+++ example/module.js
@@ -1,15 +1,17 @@
 const CommonjsPlugin = require('../lib/dependencies/CommonjsPlugin');
 const Compiler = require('../lib/Compiler');
 const WebpackOptionsApply = require('../lib/WebpackOptionsApply');
-const NodeEnviromentPlugin = require('../lib/node/NodeEnviromentPlugin');
+
 
 const compiler = new Compiler({
     context: __dirname,
 });
 
-compiler.apply(new NodeEnviromentPlugin());
-new WebpackOptionsApply().process({}, compiler);
-compiler.apply(new CommonjsPlugin());
+new WebpackOptionsApply().process({
+    output: {
+        path: 'output.js'
+    }
+}, compiler);
 
 
 compiler.compile(__dirname, './a', 'main', function () {
